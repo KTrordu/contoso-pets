542242406a84ba70df46a30a0cf86d9cfc50a6c4
KTrordu <alptug.yilmaz@outlook.com>
2024-10-05 15:58:29 +0300

    info update

--- obj/Debug/net8.0/ContosoPets.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoPets.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0919dfc03adeb4d89bec6e2c8950815a6908dbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+544af0713200b98447e775ebd80f785c9aeea3a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
